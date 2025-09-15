278e02e5277287c403f0b4b66e7fad3f85385f12
Daniel Viberg <daniel_viberg@outlook.com>
2025-09-15 18:03:44 +0200

    Tweaked docker image

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,7 @@
 cd "$(dirname "$0")"
 docker build -t vim-lsp-image .
-docker run -it -v "$(pwd)/../:/app" vim-lsp-image
+container_id=$(docker run -d -it -v "$(pwd)/../:/app" vim-lsp-image)
+docker exec -it $container_id vim -u DEFAULTS -c "source /app/plugin/lsp.vim" -c "source /app/tests/run.vim"
+docker stop $container_id
+docker rm $container_id
 docker rmi -f vim-lsp-image
